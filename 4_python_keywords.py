9a1376a523056aa83a2fa38729f9dcfa3a554cd9
Chauhanx0 <amankun70@gmail.com>
2026-02-13 21:22:11 +0530

    [notes 1]

---
+++ 4_python_keywords.py
@@ -0,0 +1,63 @@
+#keywords in python defines the rules and structure of python programs and hence you ccannot use them as variables,funtions etc. names
+
+#IDE = Integrated Development Environment
+#->IDE ek software hota hai jisme hum code likhte, run karte, aur debug karte hain — sab ek hi jagah par.
+#Integrated = Jab multiple cheezein alag-alag na ho kar ek hi system mein milkar kaam karein, use integrated kehte hain.
+
+
+# Yahan “Integrated” ka matlab:
+#     Code likhne ka editor
+#     Program run karne ka system
+#     Error check (debugger)
+#     Output window
+# Ye sab ek hi software ke andar milta hai ✅
+# Isliye ise Integrated Development Environment kehte hain.
+
+
+#Keywords vs Identifiers
+
+# Keywords
+
+# Reserved words in Python that have a specific meaning.
+# Cannot be used as variable names.
+# Examples: if, else, for, while
+# Part of the Python syntax.
+# They cannot be redefined or changed.
+
+
+
+# Identifiers
+
+# Names given to variables, functions, classes, etc.
+# Can be used as variable names if not a keyword.
+# Examples: x, number, sum, result
+# User-defined, meaningful names in the code.
+# Can be defined and redefined by the programmer.
+
+
+
+#Variables vs Keywords
+
+# Variables
+
+# Used to store data.
+# Can be created, modified, and deleted by the programmer.
+# Examples: x, age, name
+# Hold values that are manipulated in the program.
+# Variable names must follow naming rules but are otherwise flexible.
+
+
+# Keywords
+
+# Reserved words with predefined meanings in Python.
+# Cannot be modified or used as variable names.
+# Examples: if, while, for
+# Used to define the structure of Python code.
+# Fixed by Python language and cannot be altered.
+
+# Q)What keyword is used to handle exceptions in Python?
+# except
+
+# Q)Which keyword is used to create a new class in python
+# class
+
